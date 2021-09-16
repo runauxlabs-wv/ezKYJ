74acfb87f07c1fb17ebbccbb5b4b2a94c228036a
K-Ld <tp01045918974@gmail.com>
2021-09-17 00:41:14 +0900

    Index Update.

--- js/index.js
+++ js/index.js
@@ -1,40 +1,11 @@
 $(document).ready(function(){
-  $("nav .mobile-menu").click(function(){
-    $("nav ul").addClass("on");
-    $("nav .mobile-menu").css({"display": "none",});
-    $("nav .clear").css({"display": "block",});
-  });
+    $("nav ul li").each(function() {
+        var thisOffset = $("." + $(this).data('id')).offset().top;
 
-  $("nav .clear").click(function(){
-    $("nav ul").removeClass("on");
-    $("nav .mobile-menu").css({"display": "block",});
-    $("nav .clear").css({"display": "none",});
-  });
-});
-
-var swiper = new Swiper(".mySwiper-one", {
-    direction: "vertical",
-    slidesPerView: 1,
-    spaceBetween: 0,
-    mousewheel: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-
-var swiper = new Swiper(".mySwiper-two", {
-  slidesPerView: 1,
-  spaceBetween: 0,
-  // loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-  },
-  mousewheel: true,
-  keyboard: true,
+        $(this).click(function() {
+            $("html, body").animate({
+                scrollTop: thisOffset
+            }, 1000);
+        });
+    });
 });
