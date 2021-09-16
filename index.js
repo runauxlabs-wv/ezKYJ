ff7459738c47a4c3c5dddd31d169e6915270b331
K-Ld <tp01045918974@gmail.com>
2021-09-17 01:26:23 +0900

    Update Index.html

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("nav ul li").each(function() {
+        var thisOffset = $("." + $(this).data('id')).offset().top;
+
+        $(this).click(function() {
+            $("html, body").animate({
+                scrollTop: thisOffset
+            }, 1000);
+        });
+    });
+});
