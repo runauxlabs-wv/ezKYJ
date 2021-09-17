e8dd90c8c8a79f04ec72fac45d75ea84d7b9683c
K-Ld <tp01045918974@gmail.com>
2021-09-17 19:58:38 +0900

    Index Update.

--- js/index.js
+++ js/index.js
@@ -8,4 +8,15 @@ $(document).ready(function(){
             }, 1000);
         });
     });
+    $("nav .menu").click(function(){
+        $("nav ul").addClass("on");
+        $("nav .menu").css({"display": "none",});
+        $("nav .clear").css({"display": "block",});
+    });
+
+    $("nav .clear").click(function(){
+        $("nav ul").removeClass("on");
+        $("nav .menu").css({"display": "block",});
+        $("nav .clear").css({"display": "none",});
+    });
 });
